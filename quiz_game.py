dba128bd2cbac329515bfdc27bea25ccfa27d039
lachlanin4 <138771039+lachlanin4@users.noreply.github.com>
2023-07-11 14:33:20 +0100

    Add basic header for quiz game file

---
+++ quiz_game.py
@@ -0,0 +1,7 @@
+!/usr/bin/env python
+"""
+This is a an interactive quiz game using Python developed
+in groups. The game will present a series of questions to
+the player, track their score, andprovide feedback on
+their performance.
+"""
