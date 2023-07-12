efd41c5265199196fbb1a9540ec9095b4d9c0ae2
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-07-12 23:05:27 +0100

    Made Dictionary of Questions
    Made a dictionary for the questions so they can be paired with the answers

--- quiz_game.py
+++ quiz_game.py
@@ -5,3 +5,14 @@ in groups. The game will present a series of questions to
 the player, track their score, andprovide feedback on
 their performance.
 """
+
+#Makes a dictionary of questions pairing the questions and answer
+questionDict = {
+  'What is the capital of France?\nA.London\nB.Marseille\nC.Paris\n':
+  'C',
+  'What is the biggest continent in the world?\nA.Antarctica\nB.Asia\nC.North America\n':
+  'B',
+  'Which is not an element in the periodic table?\nA.Americium\nB.Volcanium\nC.Lawrencium\n':
+  'B',
+  'Which acid is in vinegar?\nA.Acetic\nB.Sulfuric\nC.Citric'
+}
